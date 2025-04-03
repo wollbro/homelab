4d608ad4798dee75ac8568a5645a7537182bfd58
Daniel Vollbro <wollbro90@gmail.com>
2025-04-03 15:54:50 +0200

    Restructure project

---
+++ nodes/gryffindor/infrastructure/provider.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "1.11.3"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.73.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = ""
+
+    endpoints = {}
+
+    access_key = ""
+    secret_key = ""
+
+    key                         = ""
+    region                      = ""
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.endpoint
+  api_token = var.api_token
+  insecure  = true
+
+  ssh {
+    agent    = true
+    username = var.ssh_user
+    password = var.ssh_password
+  }
+}
