a4affc1637062ed4eafcee98a08e73b9e76fd370
Daniel Vollbro <wollbro90@gmail.com>
2025-04-07 14:48:12 +0200

    Added mapping pci hardware

---
+++ nodes/gryffindor/infrastructure/proxmox_mapping-pci-nic2.tf
@@ -0,0 +1,16 @@
+resource "proxmox_virtual_environment_hardware_mapping_pci" "NIC2" {
+  provider = proxmox.api
+  name     = "NIC2"
+
+  map = [
+    {
+      id           = "10ec:8168"
+      iommu_group  = 31
+      node         = local.default_node
+      path         = "0000:01:00.0"
+      subsystem_id = "7470:3468"
+    },
+  ]
+
+  mediated_devices = false
+}
