f1b9de1465bfe0df0f54a3c90b4e79a7f3ebb598
Daniel Vollbro <wollbro90@gmail.com>
2025-04-07 10:57:15 +0200

    Moved cloud init vm to module

--- nodes/gryffindor/infrastructure/state.tf
+++ nodes/gryffindor/infrastructure/state.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = "1.11.1"
 
   required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.2"
-    }
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.73.1"
--- nodes/gryffindor/infrastructure/vm_1010-docker.tf
+++ nodes/gryffindor/infrastructure/vm_1010-docker.tf
@@ -2,115 +2,34 @@ data "local_file" "ssh_public_key" {
   filename = var.ssh_key_path
 }
 
-resource "proxmox_virtual_environment_file" "docker_data_cloud_config" {
-  provider = proxmox.api
-  content_type = "snippets"
-  datastore_id = "local"
-  node_name = "gryffindor"
-
-  source_raw {
-    data = <<-EOF
-    #cloud-config
-    hostname: docker
-    timezone: Europe/Stockholm
-    users:
-      - default
-      - name: ${var.docker_login_user}
-        groups:
-          - sudo
-        shell: /bin/bash
-        ssh_authorized_keys:
-          - ${trimspace(data.local_file.ssh_public_key.content)}
-        sudo: ALL=(ALL) NOPASSWD:ALL
-    package_update: true
-    packages:
-      - qemu-guest-agent
-      - net-tools
-      - curl
-    runcmd:
-      - systemctl enable qemu-guest-agent
-      - systemctl start qemu-guest-agent
-      - echo "done" > /tmp/cloud-config.done
-    EOF
-
-    file_name = "docker-data-cloud-config.yaml"
+module "vm_1010_docker" {
+  source = "./modules/cloud-init-vm"
+  providers = {
+    proxmox = proxmox.api
   }
+
+  # VM configuration
+  name = "docker"
+  vm_id = 1010
+  node = "gryffindor"
+  tags = [ "debian", "docker" ]
+
+  # Credentials
+  user = var.docker_login_user
+  ssh_keys = data.local_file.ssh_public_key.content
+
+  # Resources
+  cpu_cores = 4
+  memory = 4096
+  disk_size = 50
+
+  # Distro image configuration
+  url                = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
+  filename           = "debian-12-generic-amd64.img"
+  checksum           = "afcd77455c6d10a6650e8affbcb4d8eb4e81bd17f10b1d1dd32d2763e07198e168a3ec8f811770d50775a83e84ee592a889a3206adf0960fb63f3d23d1df98af"
+  checksum_algorithm = "sha512"
+
+  # Networking
+  ipaddress = var.docker_ip
+  gateway = var.docker_gateway
 }
-
-resource "proxmox_virtual_environment_vm" "vm_1010_docker" {
-  provider = proxmox.api
-
-  name        = "docker"
-  description = "Managed by Terraform"
-  tags        = ["terraform", "debian"]
-
-  node_name = "gryffindor"
-  vm_id     = 1010
-
-  keyboard_layout = "sv"
-
-  bios    = "ovmf"
-  machine = "q35"
-  on_boot = true
-
-  agent {
-    enabled = true
-  }
-
-  stop_on_destroy = true
-  migrate         = true
-
-  cpu {
-    cores = 4
-    type  = "host"
-    units = 100
-  }
-
-  memory {
-    dedicated = 4096
-  }
-
-  disk {
-    datastore_id = local.datastore_id
-    file_id      = module.debian_cloud_image.id
-    interface    = "virtio0"
-    iothread     = true
-    discard      = "on"
-    size         = 50
-  }
-
-  efi_disk {
-    datastore_id      = local.datastore_id
-    type              = "4m"
-    pre_enrolled_keys = false
-  }
-
-  initialization {
-    datastore_id = local.datastore_id
-    user_data_file_id = proxmox_virtual_environment_file.docker_data_cloud_config.id
-
-    ip_config {
-      ipv4 {
-        address = var.docker_ip
-        gateway = var.docker_gateway
-      }
-    }
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-  # This is required to prevent Kernel Panic error, not sure why!
-  serial_device {}
-
-  operating_system {
-    type = "l26"
-  }
-
-  tpm_state {
-    datastore_id = local.datastore_id
-    version      = "v2.0"
-  }
-}
-
