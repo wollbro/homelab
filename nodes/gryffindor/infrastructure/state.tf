e86526cb94af5cb8ac1e584c7e125fee93fc211a
Daniel Vollbro <wollbro90@gmail.com>
2025-04-07 20:51:11 +0200

    Allow other patch versions

--- nodes/gryffindor/infrastructure/state.tf
+++ nodes/gryffindor/infrastructure/state.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     local = {
       source  = "hashicorp/local"
-      version = "2.5.2"
+      version = "~> 2.5.2"
     }
 
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.73.1"
+      version = "~> 0.73.1"
     }
   }
 
