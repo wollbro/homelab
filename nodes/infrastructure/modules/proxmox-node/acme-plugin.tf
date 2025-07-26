e72a0567552334223ea4d9f90fa828079dc87134
Daniel Vollbro <wollbro90@gmail.com>
2025-07-26 15:35:02 +0200

    Moved code to separate repos

--- nodes/infrastructure/modules/proxmox-node/acme-plugin.tf
+++
@@ -1,9 +0,0 @@
-resource "proxmox_virtual_environment_acme_dns_plugin" "dns_plugin" {
-  provider = proxmox.api
-  plugin   = "cloudflare"
-  api      = "cf"
-  data = {
-    CF_Account_ID = var.cloudflare_account_id
-    CF_Token      = var.cloudflare_api_token
-  }
-}
